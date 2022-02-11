a51b089d7b3aa7744d15dc0d232ee9a172e23f4c
fwzthedev <studev170@gmail.com>
2022-02-11 10:29:48 +0100

    add division

--- index.py
+++ index.py
@@ -5,6 +5,8 @@ def addition(a,b):
 def soustraction(a,b):
     return a - b
 
+def division(a,b):
+    return a / b
 
 
 print(addition(5,5))
@@ -12,3 +14,6 @@ print(addition(5,5))
 print('------------------------------------------')
 print('Soustraction')
 print(soustraction(10,2))
+print('------------------------------------------')
+print('Division')
+print(division(100,2))
