f57d79c440087197a7e3c2736fec2419c8e8788f
fwzthedev <studev170@gmail.com>
2022-02-11 10:06:12 +0100

    add soustraction fonction

--- index.py
+++ index.py
@@ -2,5 +2,13 @@
 def addition(a,b):
     return a + b
 
+def soustraction(a,b):
+    return a - b
+
+
 
 print(addition(5,5))
+
+print('------------------------------------------')
+print('Soustraction')
+print(soustraction(10,2))
