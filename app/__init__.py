5a7827710504fc01c38b857f476b67851785b885
fwzthedev <studev170@gmail.com>
2022-02-11 09:41:56 +0100

    ajout du premier commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,24 @@
-from flask import Flask
-from flask_restful import Resource, Api
+# PYTHON IMPORT
 
+# FLASK IMPORT
+from flask import Flask
+from flask_restful import  Api
+from flask_sqlalchemy import SQLAlchemy
+
+
+#PERSONNAL IMPORT
+from .controller.user import User
+
+
+database = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     api = Api(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+   
+    database.init_app(app)
     
+    
+    api.add_resource(User, '/')
     return app
